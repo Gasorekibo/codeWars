10bff2afe5267d862b9879410c40e94aab793e35
Gasorekibo <m.gasore@alustudent.com>
2024-05-09 22:31:57 +0200

    sum of all numbers on given row

---
+++ 7kyu/sumOfOddNumbers.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
+function rowSumOddNumbers(n) {
+    //The sum of numbers on a particular row of triangle is equal to the
+    //row number power 3
+    return Math.pow(n,3) 
+  }
