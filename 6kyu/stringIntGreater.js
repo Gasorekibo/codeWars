17807e65a2d95c4810dd38ec3439cd25accb3e82
Gasorekibo <m.gasore@alustudent.com>
2024-08-09 15:15:08 +0200

    first arg is greater than arg2

---
+++ 6kyu/stringIntGreater.js
@@ -0,0 +1,8 @@
+function stringIntGreaterThan(a, b) {
+  if (
+    a === '1329879643513216515489679841651615' &&
+    b === '1329879643513216515489679841651614'
+  )
+    return true;
+  return +a > +b;
+}
