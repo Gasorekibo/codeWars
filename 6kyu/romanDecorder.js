5228a57c72d1f2d336388f3ce12780e76b64fac9
Gasorekibo <m.gasore@alustudent.com>
2024-08-23 19:50:37 +0200

    decode roman number into numeric number

---
+++ 6kyu/romanDecorder.js
@@ -0,0 +1,22 @@
+function romanNumberDecoder(str) {
+  let result = 0;
+  const romansValue = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  for (let i = 0; i < str.length; i++) {
+    const currentValue = romansValue[str[i]];
+    const nextValue = romansValue[str[i + 1]];
+    if (currentValue < nextValue) {
+      result += nextValue - currentValue;
+    } else {
+      result += romansValue[str[i]];
+    }
+  }
+  return result;
+}
